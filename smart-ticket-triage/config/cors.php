3ca5dc9f3f26cdf71983c43789957b35098140b0
JoshCodeAligator <joshdebele8@gmail.com>
2025-09-03 18:09:22 -0600

    Enabling SPA Cross-Origin Resource Sharing

---
+++ smart-ticket-triage/config/cors.php
@@ -0,0 +1,14 @@
+<?php
+
+# To Allow Cross-Origin Resource Sharing for SPA folder (Frontend Interface)
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
